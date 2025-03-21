49e6cf03ded1d27d678009d74f6682d2ad852374
M-Letting <malet21@student.sdu.dk>
2025-03-21 15:32:16 +0100

    Updates to code - rolling plots work as intended

--- app.R
+++ app.R
@@ -6,45 +6,103 @@
 #
 #    https://shiny.posit.co/
 #
-
+# Load the Shiny library and other required libraries
 library(shiny)
+library(ggplot2)
+library(DT)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+# Setting working directory as the directory where the app.R file is located
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
+#### Loading the data from plot_data folder ####
+tmt.PDC000125 <- read.csv("plot_data/tmt.PDC000125.csv", row.names=1)
+tmt.PDC000127 <- read.csv("plot_data/tmt.PDC000127.csv", row.names=1)
+tmt.PDC000153 <- read.csv("plot_data/tmt.PDC000153.csv", row.names=1)
+tmt.PDC000198 <- read.csv("plot_data/tmt.PDC000198.csv", row.names=1)
+tmt.PDC000219 <- read.csv("plot_data/tmt.PDC000219.csv", row.names=1)
+tmt.PDC000234 <- read.csv("plot_data/tmt.PDC000234.csv", row.names=1)
+tmt.PDC000270 <- read.csv("plot_data/tmt.PDC000270.csv", row.names=1)
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+#### Get custom functions from the helper file ####
+source("helper.R")
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+#### Define tmt data as a list ####
+dataset_names <- c(
+  "PDC000125 - Uterine" = "tmt.PDC000125",
+  "PDC000127 - Renal" = "tmt.PDC000127",
+  "PDC000153 - Lung" = "tmt.PDC000153",
+  "PDC000198 - Hepatic" = "tmt.PDC000198",
+  "PDC000219 - Lung" = "tmt.PDC000219",
+  "PDC000234 - Lung" = "tmt.PDC000234",
+  "PDC000270 - Pancreas" = "tmt.PDC000270"
 )
 
-# Define server logic required to draw a histogram
+#### Define UI ####
+ui <- fluidPage(
+  titlePanel("Protein Expression Across Age"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("dataset", "Select dataset", choices = dataset_names),
+      textInput("GOI", "Enter Genes of Interest (GOI) as a comma-separated list",
+                placeholder = "Enter GOI here..."),
+      radioButtons("binned", "Select Plot Type", 
+                   choices = c("Binned plot", "Rolling plot")),
+      
+      # Conditional options for Binned plot
+      conditionalPanel(
+        condition = "input.binned == 'Binned plot'",
+        checkboxGroupInput("bin_plot_include", 
+                           "What should be included in the plot?",
+                           choices = c("Scatter", "Line", "Error Bars", "Point")),
+      ),
+      conditionalPanel(
+        condition = "input.binned == 'Rolling plot'",
+        numericInput("window_size", 
+                     "Enter window size for rolling plot", 
+                     value = 5)
+      ),
+      actionButton("update_btn", "Update Table"),
+    ),
+    mainPanel(
+      plotOutput("plot")
+    )
+  )
+)
+
+
+# Define server logic
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  # Reactive expression triggered only when button is clicked
+  filtered_plot <- eventReactive(input$update_btn, {
+    req(input$dataset)  # Ensure a dataset is selected
+    dataset <- get(input$dataset, envir = .GlobalEnv)  # Fetch dataset
+    
+    # Process Genes of Interest
+    GOI <- trimws(unlist(strsplit(input$GOI, ",")))  # Split by comma
+    
+    # Filter dataset for selected genes and last 2 columns
+    dataset_filtered <- dataset[, which(colnames(dataset) %in% GOI)]
+    
+    dataset_filtered$Age <- dataset$Age
+    dataset_filtered$AgeBin <- dataset$AgeBin
+    
+    # Create desired plot using helper functions based on user input
+    p1 <- if (input$binned == "Rolling plot") {
+      create_incremental_plot(dataset, input$window_size,
+                              genes = GOI,
+                              create_plot = TRUE)
+    } else {
+      plot_rolling(dataset, input$window_size)
+    }
+    
+    return(p1)
+  })
+  
+  # Render the plot of the filtered dataset
+  output$plot <- renderPlot({
+    req(filtered_plot())  # Ensure reactive plot exists
+    filtered_plot()  # Call the reactive expression to return plot
+  })
 }
 
 # Run the application 
---
+++ helper.R
@@ -0,0 +1,141 @@
+###############################################################################
+# Functions used in ISA
+###############################################################################
+
+# Required pachages
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+library(RColorBrewer)
+
+# Create a dataframe with sd, median, mean age for each gene for each age bin
+# Input: Dataframe with gene expression data, age bins
+create_EB_df <- function(df){
+  sd_df <- data.frame()
+  for (gene in colnames(df[,-c(ncol(df),ncol(df)-1)])){
+    for (age_bin in unique(df$AgeBin)){
+      sd_df <- rbind(sd_df, data.frame(
+        Gene = gene,
+        AgeBin = age_bin,
+        MeanAge = mean(as.numeric(unlist(regmatches(age_bin, 
+                                                    gregexpr("[0-9]+", age_bin))))),
+        Median = median(df[df$AgeBin == age_bin, gene],
+                        na.rm = TRUE),
+        SD = sd(df[df$AgeBin == age_bin, gene],
+                na.rm = TRUE)
+      ))
+    }
+  }
+  return(sd_df)
+}
+
+# Create ggplot object given a tmt. dataframe, an EB dataframe, and a list
+# of genes
+# Input: TMT dataframe, EB dataframe, list of genes
+create_age_EB_plot <- function(tmt_df, EB_df, genes, alfa = 0.5,
+                               scatter = TRUE,
+                               line = TRUE, 
+                               EB = TRUE,
+                               point = TRUE) {
+  # Create an empty ggplot object
+  p <- ggplot()
+  
+  for (gene in genes) {
+    # Create a temporary dataframe with the gene label
+    tmt_df_gene <- tmt_df %>% mutate(Gene = gene)
+    EB_df_gene <- EB_df[EB_df$Gene == gene, ] %>% mutate(Gene = gene)
+    
+    if (scatter == TRUE) {
+      p <- p + geom_point(data = tmt_df_gene, aes(x = Age, 
+                                                  y = .data[[gene]], 
+                                                  color = Gene),
+                          size = 1, 
+                          alpha = alfa)
+    }
+    
+    if (point == TRUE) {
+      p <- p + stat_summary_bin(data = tmt_df_gene, aes(x = Age, 
+                                                        y = .data[[gene]], 
+                                                        color = Gene),
+                                fun = "median", 
+                                geom = "point", 
+                                binwidth = 10,
+                                size = 1.5)
+    }
+    
+    if (line == TRUE) {
+      p <- p + stat_summary_bin(data = tmt_df_gene, aes(x = Age, 
+                                                        y = .data[[gene]], 
+                                                        color = Gene),
+                                fun = "median", 
+                                geom = "line", 
+                                binwidth = 10)
+    }
+    
+    if (EB == TRUE) {
+      p <- p + geom_errorbar(data = EB_df_gene, aes(x = MeanAge, 
+                                                    ymin = Median - SD, 
+                                                    ymax = Median + SD, 
+                                                    color = Gene),
+                             width = 2)
+    }
+  }
+  
+  p <- p + scale_color_brewer(palette = "Set1") +
+    labs(x = "Age", 
+         y = "Log Ratio")
+  
+  return(p)
+}
+
+# Create rolling plot (mean of i - h to i + h) for a list of genes
+# Input: TMT dataframe, list of genes, h (window size)
+# Options: return_df (return a dataframe), create_plot (create a plot)
+create_incremental_plot <- function(tmt_df, 
+                                    genes, 
+                                    h = 2,
+                                    return_df = FALSE,
+                                    create_plot = TRUE) {
+  # Error handling
+  if (return_df == TRUE & create_plot == TRUE) {
+    stop("You can only return a dataframe or create a plot, not both")
+  }
+  
+  # Order tmt data based on age (low to high)
+  tmt_df <- tmt_df[order(tmt_df$Age),]
+  
+  # Create an empty dataframe
+  res_df <- data.frame(matrix(ncol = length(genes) + 1, 
+                              nrow = nrow(tmt_df) - 2 * h))
+  colnames(res_df) <- c("Age", genes)
+  
+  # Fill df with the incremental values
+  for (i in (1 + h):(nrow(tmt_df) - h)){
+    res_df$Age[i - h] <- mean(tmt_df$Age[(i - h):(i + h)])
+  }
+  
+  for (gene in genes) {
+    for (i in (1 + h):(nrow(tmt_df) - h)){
+      res_df[i - h, gene] <- mean(tmt_df[[gene]][(i - h):(i + h)])
+    }
+  }
+  
+  if (return_df == TRUE) {
+    return(res_df)
+  } 
+  
+  if (create_plot == TRUE) {
+    # Reshape data to long format (so ggplot can correctly assign colors)
+    res_df_long <- res_df %>%
+      pivot_longer(cols = -Age, names_to = "Gene", values_to = "Value")
+    
+    # Create plot
+    p <- ggplot(res_df_long, aes(x = Age, y = Value, color = Gene)) + 
+      geom_point(alpha = 0.6) +
+      scale_color_brewer(palette = "Set1") +
+      labs(x = "Age", y = "Log Ratio")
+    
+    return(p)
+  }
+}
+
